41d01eae8ad734293841b6ce224e4bcb25be109b
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-12-16 20:34:45 -0500

    rough edges of card

---
+++ src/Card.java
@@ -0,0 +1,41 @@
+public class Card {
+    private String rank;
+    private String suit;
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    public String getRank() {
+        return rank;
+    }
+    public String getSuit() {
+        return suit;
+    }
+    @Override
+    public String toString() {
+        return rank + suit;
+    }
+    public static int getOrderedRank(String rank) {
+        try {
+            return Integer.parseInt(rank);
+        } catch (NumberFormatException e) {
+            switch (rank) {
+                case "T": return 10;
+                case "J": return 10;
+                case "Q": return 10;
+                case "K": return 10;
+                case "A": return 11;
+            }
+        }
+        return -1;
+    }
+    public static int getOrderedSuit(String suit) {
+        switch (suit) {
+            case "C": return 1;
+            case "D": return 2;
+            case "H": return 3;
+            case "S": return 4;
+        }
+        return -1;
+    }
+}
